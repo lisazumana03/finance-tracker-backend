194290ddb1138729b841d919e5612300bae489f2
Lisa Zumana <zumanalisa@gmail.com>
2026-05-02 22:58:54 +0200

    file incoming

---
+++ src/main/java/za/co/lzinc/controller/authentication/UserController.java
@@ -0,0 +1,4 @@
+package za.co.lzinc.controller.authentication;
+
+public class UserController {
+}
---
+++ src/main/java/za/co/lzinc/domain/authentication/User.java
@@ -0,0 +1,110 @@
+package za.co.lzinc.domain.authentication;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private LocalDateTime creationDate;
+    private String currency;
+
+    private User(Builder builder){
+        this.userId = builder.userId;
+        this.fullName = builder.fullName;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.phoneNumber = builder.phoneNumber;
+        this.creationDate = builder.creationDate;
+        this.currency = builder.currency;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", creationDate=" + creationDate +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private int userId;
+        private String fullName;
+        private String email;
+        private String password;
+        private String phoneNumber;
+        private LocalDateTime creationDate;
+        private String currency;
+
+        public Builder setUserId(int userId){
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setFullName(String fullName){
+            this.fullName = fullName;
+            return this;
+        }
+
+        public Builder setEmail(String email){
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPassword(String password){
+            this.password = password;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder setCreationDate(LocalDateTime creationDate){
+            this.creationDate = creationDate;
+            return this;
+        }
+
+        public Builder setCurrency(String currency){
+            this.currency = currency;
+            return this;
+        }
+
+    }
+}
