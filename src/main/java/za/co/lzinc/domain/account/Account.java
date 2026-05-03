0626612adac62b8c62f05f211f81edbf5e261b1b
Lisa Zumana <zumanalisa@gmail.com>
2026-05-03 13:16:52 +0200

    file incoming

--- src/main/java/za/co/lzinc/domain/account/Account.java
+++ src/main/java/za/co/lzinc/domain/account/Account.java
@@ -13,8 +13,36 @@ public class Account {
     private LocalDateTime creationDate;
 
     private Account (Builder builder){
+        this.accountId = builder.accountId;
+        this.userId = builder.userId;
         this.accountName = builder.accountName;
+        this.accountType = builder.accountType;
         this.accountBalance = builder.accountBalance;
+        this.creationDate = builder.creationDate;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
     }
 
     public static class Builder{
@@ -24,5 +52,49 @@ public class Account {
         private AccountType accountType;
         private double accountBalance; //sensitive data
         private LocalDateTime creationDate;
+
+        public Builder setAccountId(String accountId){
+            this.accountId = accountId;
+            return this;
+        }
+
+        public Builder userId(User userId){
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setAccountName(String accountName){
+            this.accountName = accountName;
+            return this;
+        }
+
+        public Builder setAccountType(AccountType accountType){
+            this.accountType = accountType;
+            return this;
+        }
+
+        public Builder setAccountBalance(double accountBalance){
+            this.accountBalance = accountBalance;
+            return this;
+        }
+
+        public Builder setCreationDate(LocalDateTime creationDate){
+            this.creationDate = creationDate;
+            return this;
+        }
+
+        public Builder copy(Account account){
+            this.accountId = account.accountId;
+            this.userId = account.userId;
+            this.accountName = account.accountName;
+            this.accountType = account.accountType;
+            this.accountBalance = account.accountBalance;
+            this.creationDate = account.creationDate;
+            return this;
+        }
+
+        public Account build(){
+            return new Account(this);
+        }
     }
 }
--- src/main/java/za/co/lzinc/domain/account/Budget.java
+++ src/main/java/za/co/lzinc/domain/account/Budget.java
@@ -7,18 +7,54 @@ import java.time.LocalDateTime;
 public class Budget {
     private String budgetId;
     private User userId;
-    private String categoryId;
+    private Category categoryId;
     private double limit;
     private Period period;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
-    private Budget(Builder builder){}
+    private Budget(Builder builder){
+        this.budgetId = builder.budgetId;
+        this.userId = builder.userId;
+        this.categoryId = builder.categoryId;
+        this.limit = builder.limit;
+        this.period = builder.period;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+    }
+
+    public String getBudgetId() {
+        return budgetId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public Category getCategoryId() {
+        return categoryId;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public Period getPeriod() {
+        return period;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
 
     public static class Builder{
         private String budgetId;
         private User userId;
-        private String categoryId;
+        private Category categoryId;
         private double limit;
         private Period period;
         private LocalDateTime startDate;
